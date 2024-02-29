9415f0dc0c1a32406c44a68b1b5371e073aefd5a
amir mirfallahi <mirfallahi2009@gmai.com>
2024-02-29 18:14:55 +0330

    Configure Static Files: collectstatic

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +9,4 @@ urlpatterns = [
     path('', include(('home.urls', 'home'), namespace='home')),
     path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+
