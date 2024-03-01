73f0ebc833fcc132636ef5403c5ba7e6743abef2
amir mirfallahi <mirfallahi2009@gmai.com>
2024-03-01 09:57:30 +0330

    Fix configuration_manage.py

--- home/templatetags/configuration_manage.py
+++ home/templatetags/configuration_manage.py
@@ -8,18 +8,21 @@ register = template.Library()
 @register.simple_tag
 def get_config(part):
     config = Config.objects.first() or None
-    match part:
-        case "address":
-            return config.address
-        case "phone":
-            return config.phone
-        case "email":
-            return config.email
-        case "logo":
-            return config.logo.url
-        case "keywords":
-            return config.keywords
-        case "author":
-            return config.author
-        case "description":
-            return config.description
+    try:
+        match part:
+            case "address":
+                return config.address
+            case "phone":
+                return config.phone
+            case "email":
+                return config.email
+            case "logo":
+                return config.logo.url
+            case "keywords":
+                return config.keywords
+            case "author":
+                return config.author
+            case "description":
+                return config.description
+    except AttributeError:
+        return ""
